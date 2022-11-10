1fdb64417215be83dbaabdc76ace553ad9f8c3e4
yomZsamora <samaurah@gmail.com>
2022-11-10 17:08:36 +0300

    Hidden Sensitive Info using python-decouple.

--- flights/settings.py
+++ flights/settings.py
@@ -74,26 +74,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'flights.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'flights_db',
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'flights_db',
-        'USER': 'yomz',
-        'PASSWORD': '@dZumi0991',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': '3306',
     }
